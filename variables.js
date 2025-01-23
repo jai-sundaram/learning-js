48196906ef63138fcc5098b558d0d808496802aa
jaivatsan-elango-sundaram <je.sundaram@gmail.com>
2025-01-23 15:19:14 -0500

    learned variables in js

--- variables.js
+++ variables.js
@@ -47,6 +47,20 @@ console.log(favoriteSmn);
 //the symbol data type is used to create unique identifiers and objects 
 const uniqueKey = Symbol();
 
+
+
+//we can use the typeof command  to figure out the data type for a certain value 
+
+//javascript is dynamically typed, meaning that a variable intially intialized with a certain data type CAN be set to a value of a different data type 
+
+let testing = 'x';
+console.log(typeof testing);
+testing = 1;
+console.log(typeof testing);
+testing = false;
+console.log(typeof testing);
+
+//objects
 //creating an object    
 //btw objects have a key value pair 
 //in this case name is the key, hours is the value
@@ -54,5 +68,33 @@ let course = {
     name: 'Javascript for Beginnners',
     hours: 3
 }
+//the key and the value are considered to be part of the object
+console.log(course);
+//can specifically output either the key or the value
+console.log(course.name);
+console.log(course.hours);
+//can also reassign the properties
+course.name = 'javascript course';
+console.log(course.name);
 
-//we can use the typeof command in the terminal to figure out the data type for a certain value 
+//can also use bracket notation
+console.log(course['name']);
+///can also use the bracket notation to update values
+course['name'] = 'Javascript Fundamentals';
+console.log(course['name']);
+
+
+
+//arrays
+let productColors = ['blue', 'green'];
+//accessing one element 
+//like java access by index, index starts at 0
+console.log(productColors[0]);
+//can have different data types in one array
+productColors[1] = 9;
+console.log(productColors[1]);
+//datatype of an array is object
+
+
+//getting the length of an array
+console.log(productColors.length);
