22abaf131a8f5977b5ff70cf5fb2c5082952311a
jaivatsan-elango-sundaram <je.sundaram@gmail.com>
2025-01-23 23:05:41 -0500

    looping

--- conditional_statements_and_loops.js
+++ conditional_statements_and_loops.js
@@ -34,3 +34,104 @@ switch(job){
     default:
         console.log('Works directly with customers');
 }
+
+//for loops
+let numbers = [1, 2, 3, 4, 5, 6, 7]
+
+//want to acess each value by index
+for (let idx = 0; idx < numbers.length; idx++){
+    console.log(numbers[idx]);
+}
+/*
+for(let num = 0; idx < 100; i++){
+    console.log(num);
+}*/
+
+
+//while loops
+let idx = 0;
+while(idx < numbers.length){
+    console.log(numbers[idx]);
+    idx++;
+}
+//another example of while loops 
+let sum = 0;
+while(true){
+    console.log('Loop');
+    sum++;
+    //if the sum is greater than 10, break out of the loop  
+    if(sum == 10){
+        break;
+    }
+}
+
+//do while loops
+//not widely used, but good to be aware of it 
+//in a regular while loop, we first check the condition then we do the action
+//in a do while loop, we do the action and then check the condition 
+let i = 0;
+do {
+    console.log(i);
+    i++;
+}
+while(i < 10);
+
+
+
+//ALWAYS MAKE SURE THE LOOPS TERMINATE
+
+
+//for in loop
+
+//in js, we have a syntax that also allows us to iterate through the keys of a javascript object   
+const course = {
+    name: "JavaScript for beginners",
+    duration: 3,
+    sections: 7
+}
+//the for in loop is used with javascript objects to allows us to iterate over the javascript object and get acess to the keys 
+//will retrieve all the keys in the course 
+for(const key in course){
+    console.log(course[key]);
+}
+
+//for of loop 
+//usually used when iterating over an array, will be more simple 
+//element in this case is the actual element in the list, not the index 
+console.log('printing using for of loop');
+for(let element of numbers){
+    console.log(element);
+}
+
+//break and continue statements 
+//continue statement means that u want to skip the current iteration of the loop
+//move on to the next iteration of the loop 
+console.log('using continue statement');
+for(let i = 0; i < 10; i++){
+    if (i%2 == 0){
+        continue;
+    }
+    console.log(i);
+}
+//another example
+console.log('another example');
+let j = 0;
+while(j < 10){
+    j++;
+    if(j%2==0){
+        continue;
+    }
+    console.log(j);
+}
+let obk = {
+    a: 1,
+    b: 2,
+    c: 3
+}
+//final exampls 
+for(const key in obk){
+    if(key == 'b'){
+        continue;
+    }
+    console.log(obk[key]);
+}
